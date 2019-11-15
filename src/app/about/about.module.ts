de42fbffc2957d278d2a7ff302b6cff3e0b420fb
Ruben Schuckit <rschuckit@wustl.edu>
2019-11-15 17:23:02 -0600

    Switched to NGX markdown and added spinner for slow connections when … (#10)
    * Switched to NGX markdown and added spinner for slow connections when converting to MD.
    
    * Fixed about site component test + committed files I forgot to stage.

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AboutSiteComponent } from './about-site.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AboutSiteComponent } from "./about-site.component";
+import { MarkdownModule } from "ngx-markdown";
+import { HttpClient } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AboutSiteComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MarkdownModule.forRoot({ loader: HttpClient })
   ]
 })
 export class AboutModule {
